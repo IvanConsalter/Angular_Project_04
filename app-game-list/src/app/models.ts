a1a969341600bbb8739e97bb0d51a35ec47de3c3
Ivan Consalter <ivanconsalter1@gmail.com>
2022-01-20 09:39:31 -0300

    added models.ts

---
+++ app-game-list/src/app/models.ts
@@ -0,0 +1,50 @@
+export interface Game {
+
+  background_img: string;
+  name: string;
+  released: string;
+  metacritic_url: string;
+  website: string;
+  description: string;
+  metacritic: number;
+  genres: Array<Genre>;
+  parent_platforms: Array<ParentPlatform>;
+  publishers: Array<Publishers>;
+  ratings: Array<Rating>;
+  screenshots: Array<Screenshot>;
+  trailers: Array<Trailer>;
+}
+
+export interface APIResponse<T> {
+  result: Array<T>;
+}
+
+interface Genre {
+  name: string;
+}
+
+interface ParentPlatform {
+  platform: {
+    name: string;
+  };
+}
+
+interface Publishers {
+  name: string;
+}
+
+interface Rating {
+  id: number;
+  count: number;
+  title: string;
+}
+
+interface Screenshot {
+  image: string;
+}
+
+interface Trailer {
+  data: {
+    max: string;
+  };
+}
