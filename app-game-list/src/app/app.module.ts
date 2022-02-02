83f2d674cd4fed643cae7fd41e70255c0d055e57
Ivan Consalter <ivanconsalter1@gmail.com>
2022-02-02 10:01:11 -0300

    added route to details page

--- app-game-list/src/app/app.module.ts
+++ app-game-list/src/app/app.module.ts
@@ -20,12 +20,14 @@ import { SearchBarComponent } from './components/search-bar/search-bar.component
 import { HomeComponent } from './components/home/home.component';
 import { HttpHeadersInterceptor } from './interceptors/http-headers.interceptor';
 import { HttpErrorsInterceptor } from './interceptors/http-errors.interceptor';
+import { DetailsComponent } from './components/details/details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchBarComponent,
-    HomeComponent
+    HomeComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
--- app-game-list/src/app/components/home/home.component.ts
+++ app-game-list/src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { APIResponse, Game } from 'src/app/models';
 import { HttpService } from 'src/app/services/http.service';
 
@@ -8,18 +9,21 @@ import { HttpService } from 'src/app/services/http.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
   public sort: string;
   public games: Array<Game> = [];
+  private routeSub: Subscription;
+  private gameSub: Subscription;
 
   constructor(
     private httpService: HttpService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe((params: Params) => {
+    this.routeSub = this.activatedRoute.params.subscribe((params: Params) => {
       if(params['game-search']) {
         this.searchGames('metacrit', params['game-search']);
       }
@@ -30,7 +34,7 @@ export class HomeComponent implements OnInit {
   }
 
   searchGames(sort: string, search?: string): void {
-    this.httpService
+    this.gameSub = this.httpService
       .getGameList(this.sort, search)
       .subscribe((gameList: APIResponse<Game>) => {
         this.games = gameList.results;
@@ -38,6 +42,18 @@ export class HomeComponent implements OnInit {
       });
   }
 
+  openGameDetails(id: string): void {
+    this.router.navigate(['details', id]);
+  }
 
+  ngOnDestroy(): void {
+    if(this.gameSub) {
+      this.gameSub.unsubscribe();
+    }
+
+    if(this.routeSub) {
+      this.routeSub.unsubscribe();
+    }
+  }
 
 }
