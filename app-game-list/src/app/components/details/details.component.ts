d309e3da2386c275a7629191480616c35761615e
Ivan Consalter <ivanconsalter1@gmail.com>
2022-02-07 08:25:12 -0300

    added method to get game details in details.component.ts

--- app-game-list/src/app/components/details/details.component.ts
+++ app-game-list/src/app/components/details/details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { Game } from 'src/app/models';
+import { HttpService } from 'src/app/services/http.service';
 
 @Component({
   selector: 'app-details',
@@ -10,16 +12,30 @@ import { Subscription } from 'rxjs';
 export class DetailsComponent implements OnInit {
 
   gameId: string;
+  game: Game;
   routeSub: Subscription;
+  gameSub: Subscription;
 
   constructor(
     private activatedRoute: ActivatedRoute,
+    private httpService: HttpService
   ) { }
 
   ngOnInit(): void {
     this.routeSub = this.activatedRoute.params.subscribe( (params: Params) => {
-      this.gameId = params.id;
+      this.gameId = params.id;      
+      this.getGameDetails(this.gameId);
     });
+  }
+
+  getGameDetails(gameId: string): void {
+    this.gameSub = this.httpService
+      .findGameById(gameId)
+      .subscribe( (gameResponse: Game) => {
+        this.game = gameResponse;
+        console.log(this.game);
+        
+      })
   }
 
 }
