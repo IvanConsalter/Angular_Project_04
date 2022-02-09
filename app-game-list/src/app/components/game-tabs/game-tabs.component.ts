32507e23b2676a68377b304fbf7b063667eee79a
Ivan Consalter <ivanconsalter1@gmail.com>
2022-02-09 15:08:01 -0300

    sharing data between game-tabs & details component with @Input()

--- app-game-list/src/app/components/game-tabs/game-tabs.component.ts
+++ app-game-list/src/app/components/game-tabs/game-tabs.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Game } from 'src/app/models';
 
 @Component({
   selector: 'app-game-tabs',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-tabs.component.scss']
 })
 export class GameTabsComponent implements OnInit {
+
+  @Input() game: Game; 
 
   constructor() { }
 
